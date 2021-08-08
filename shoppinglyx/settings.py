fe516a0e6bb9a41c26b8f130defa40e52c98bab9
visheshsolanki12345 <visheshsolanki12345@gmail.com>
2021-08-08 19:33:48 +0530

    second commit

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '8o353)z(_*^04+jhey!m*q3_no@vhn*7v8(6xz4rl6=kunu=it'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
