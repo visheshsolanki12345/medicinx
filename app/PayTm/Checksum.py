a8ac6565281923c9f990b897a53f7fcfb0d0de9f
visheshsolanki12345 <visheshsolanki12345@gmail.com>
2021-08-09 11:54:48 +0530

    eight commit

--- app/PayTm/Checksum.py
+++ app/PayTm/Checksum.py
@@ -122,14 +122,14 @@ def __decode__(to_decode, iv, key):
 
 if __name__ == "__main__":
     params = {
-        'MID': 'SWKkph05465739185211',
+        'MID': 'zgZiaS95597255328985',
         'ORDER_ID': 'order.order_id',
         'TXN_AMOUNT': '1',
         'CUST_ID': 'email',
         'INDUSTRY_TYPE_ID': 'Retail',
         'WEBSITE': 'WEBSTAGING',
         'CHANNEL_ID': 'WEB',
-        'CALLBACK_URL':'http://127.0.0.1:8000/handlepayment/',
+        'CALLBACK_URL':'https://medicinex.herokuapp.com/handlepayment/',
     }
 
     print(verify_checksum(
