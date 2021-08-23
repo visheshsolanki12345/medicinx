aace73680512a6ced78754759f106606b6fe10e1
visheshsolanki12345 <visheshsolanki12345@gmail.com>
2021-08-23 18:02:51 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -347,6 +347,7 @@ def payment_done(request):
             'WEBSITE': 'WEBSTAGING',
             'CHANNEL_ID': 'WEB',
             'CALLBACK_URL':'https://medicinex.herokuapp.com/handlepayment/',
+            # 'CALLBACK_URL':'http://127.0.0.1:8000/handlepayment/',
 
         }
         param_dict['CHECKSUMHASH'] = Checksum.generate_checksum(param_dict, MERCHANT_KEY)
